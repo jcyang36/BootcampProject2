8e3ec7c7be19f1e81b4d87c9029e6c0f22309d9e
Joyce <jcyang@hmc.edu>
2017-05-24 16:43:05 -0400

    Add bday java program to product line

---
+++ bday.java
@@ -0,0 +1,26 @@
+
+public class bday{
+	public static void main(String[] args){
+		int bday=0;
+		int bMonth=0;
+		int bYear=0;
+		int currentDay=24;
+		int currentMonth=5;
+		int currentYear=2017;
+		int age=0;
+		int ageTurning=0;
+		char repeat='N';
+		boolean flag=false;
+		do{ 
+
+			do{
+				if (bYear!=(int)bYear){
+					flag=true;
+				}
+
+				 do{ System.out.print( "This is a test.");
+				}while(bYear>currentYear);
+			}while(bYear!=(int)bYear  );
+		}while(repeat=='Y');
+	}
+}
