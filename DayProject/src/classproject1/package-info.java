e97f179170026869ceac08e8f7794ea4cb880c81
Joyce <jcyang@hmc.edu>
2017-05-26 16:26:38 -0400

    First commit

---
+++ DayProject/src/classproject1/package-info.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+/**
+ * @author student
+ *
+ */
+package classproject1;
+import java.util.Scanner;
+
+public class Hello406Again {
+	public static void main (String[] args){
+	
+		System.out.println("Enter a number between 1 and 12");
+		Scanner scannerInput=new Scanner(System.in);
+		int randomNumber = scannerInput.nextInt();
+		if(randomNumber==1){
+			System.out.println("31");
+		}else if (randomNumber==2){
+			System.out.println("28");
+		}else if (randomNumber==3){
+			System.out.println("31");
+		}else if (randomNumber ==4){
+			System.out.println("30);
+		}else if (randomNumber ==5) {
+			System.out.println("31");
+		}else if(randomNumber ==6){
+			System.out.println("30");
+		}else if (randomNumber ==7){
+			System.out.println("31");
+		}else if (randomNumber==8){
+			System.out.println("31")
+		}else if (randomNumber==9){
+			System.out.println("30");
+		}else if (randomNumber ==10){
+			System.out.println("31");
+		}else if (randomNumber ==11){
+			System.out.println("30");
+		}else if (randomNumber ==12){
+			System.out.println("31");
+		}else {
+			System.out.println("Please enter a number between 1 and 12");
+		}
+				
+				
+	}
+}
+
+
--- README.md
+++
@@ -1,1 +0,0 @@
-This project is a program that inputs the day, month, and year of birth of the user and outputs the age of the user.
